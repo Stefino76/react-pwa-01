e377d8c8afbedfa43fedcc3a832a6c6263aaa5c5
Stefano Baccetti <stefino76@live.com>
2021-03-23 23:44:44 +0100

    Aggiunto css tailwind

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import KimiaButton from './components/KimiaButton';
+import {ToastPage} from './components/KimiaToast';
+import {Spinner} from './components/elements/KimiaSpinner'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <KimiaButton color="primary">Primary</KimiaButton>
+      <KimiaButton color="primary" >Primary</KimiaButton>
+      <ToastPage /> 
+      <Spinner color="#1d4ed8" className="h-16" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
